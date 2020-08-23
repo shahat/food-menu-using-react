ec3758d3f1eb73b13be6f788a384abbb36975fab
shahat <me6847570@gmail.com>
2020-08-23 16:01:15 +0200

    the first commit in 23/8/2020 by mohamed elshahat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment  } from 'react';
+import Menu from "./components/menu";
+import Dishes from "./shared/dishes"
+class App extends Component {
+  state = { 
+    dishes:Dishes
+   }
+  render() { 
+    return (
+      <Fragment>
+         <Menu  dishes={this.state.dishes} />
+      </Fragment>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      );
+  }
 }
-
+ 
 export default App;
