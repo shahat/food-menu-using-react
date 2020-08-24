17cc14bd3f9b39d94f06677ec7d002bc8d89a444
shahat <me6847570@gmail.com>
2020-08-24 05:38:42 +0200

    completed in 24/8/2020

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,20 +1,19 @@
 import React , { Component } from 'react';
-import { Media } from 'reactstrap';
+import { Card ,CardBody ,CardImg,CardImgOverlay,CardText,CardTitle } from 'reactstrap';
 class Menu extends Component {
   
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={dish.image} alt={dish.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{dish.name}</Media>
+              <div key={dish.id} className="col-12 col-md-5 mt-1">
+                <Card tag="li">
+                  
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
                         <p>{dish.description}</p>
-                    </Media>
-                </Media>
+                    </CardImgOverlay>
+                </Card>
               </div>
             );
         });
@@ -22,9 +21,7 @@ class Menu extends Component {
         return (
           <div className="container">
             <div className="row">
-              <Media list>
                   {menu}
-              </Media>
             </div>
           </div>
         );
